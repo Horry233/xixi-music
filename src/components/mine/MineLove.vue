95a02d2f5bf7e0f6c05f3ba037577947de7e17fd
HORRY <308366974@qq.com>
2021-01-26 12:05:40 +0800

    commit

---
+++ src/components/mine/MineLove.vue
@@ -0,0 +1,97 @@
+<template>
+    <transition name="up">
+        <div class="mine-list">
+            <div class="back" @click="back">
+                <i class="icon-back"></i>
+                <span class="title">{{title}}</span>
+            </div>
+            <scroll :list="list" class="song-scroll" ref="scroll">
+                <div>
+                    <music-item :songs="list"></music-item>
+                </div>
+            </scroll>
+            <no-result v-show="list.length == 0 || list == undefined" class="noResult"></no-result>
+        </div>
+    </transition>
+</template>
+<script>
+import Scroll from '../base/Scroll'
+import NoResult from '../../components/result/NoResult'
+import MusicItem from '../list/MusicItem'
+import { playListMixin } from '../../common/js/mixin'
+import { mapState } from 'vuex'
+export default {
+    mixins: [ playListMixin ],
+    computed:{
+        ...mapState({
+            loveMusic: 'loveMusic',
+            historyPlay: 'historyPlay'
+        }),
+        title() {
+            return this.$route.name == 'mineLove' ? '我喜欢': '历史播放'
+        },
+        list() {
+            return this.$route.name == 'mineLove' ? this.loveMusic: this.historyPlay
+        }
+    },
+    components: {
+        Scroll,
+        NoResult,
+        MusicItem
+    },
+    methods: {
+        handlePlaylist(playlist) {
+            const bottom = playlist.length > 0 ? '60px' : ''
+            // list是个vue components，.$el取元素
+            this.$refs.scroll.$el.style.bottom = bottom
+            this.$refs.scroll.refresh()
+        },
+        back() {
+            this.$router.back()
+        },
+        // 点击歌曲进行播放
+        selectItem(index) {
+            this.$store.commit('setCurrentIndex', index) // 传递当前播放歌曲索引
+            this.$store.commit('setPlayList', this.list) // 传递当前播放歌曲列表
+            this.$store.commit('setSequenceList', this.list) // 传递顺序播放列表
+            this.$store.commit('setFullScreen',true) // 传递当前播放歌曲列表
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+@import "../../common/stylus/variable.styl"
+.up-enter-active,.up-leave-active
+    opacity 1
+    transition: all .5s;
+.up-enter, .up-leave-to
+    opacity: 0;
+    transform: translateX(100%);
+.mine-list
+    position fixed
+    width 100%
+    top 0
+    bottom 0
+    z-index 11
+    background-color $color-background
+    .back
+        height 40px
+        display flex
+        align-items center
+        padding-left 10px
+        i
+            font-size 22px
+            color $color-theme
+        span
+            margin-left 10px
+    .song-scroll
+        position fixed
+        overflow hidden
+        top 40px
+        bottom 0
+        width 100%
+        padding 20px 20px 0
+        .noResult
+            top 40px
+
+</style>
---
+++ src/views/rank/Rank.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="rank" ref="rank">
+		<rank-list :ranklist="ranklist"></rank-list>
+        <div class="loading-wrapper" v-show="!ranklist.length">
+            <loading/>
+        </div>
+	</div>
+</template>
+
+<script>
+import Loading from '../../components/base/Load'
+import RankList from '../../components/rank/RankList'
+export default {
+    name: 'rank',
+    components: {
+        Loading,
+        RankList
+    },
+    data () {
+        return {
+            ranklist: []
+        }
+    },
+    methods: {
+        async fetchRank () {
+            const res = await this.$http.get('/api/top/category', { params: { showDetail: 1 } })
+            /* 处理一下数据 */
+            this.ranklist = res.data.data.reduce((total, item) => total.concat(item.list), [])
+        }
+    },
+    created () {
+        this.fetchRank()
+    }
+}
+</script>
+
+<style scoped lang="stylus">
+@import '../../common/stylus/variable.styl'
+.rank
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0 
+    .loading-wrapper
+        position: absolute
+        width: 100%
+        top: 50%
+        transform: translateY(-50%)
+</style>
